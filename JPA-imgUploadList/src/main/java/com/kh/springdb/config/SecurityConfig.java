3f7ef9f60c73acc55c654a66ea190caac3e7ebc4
hiiiiiilim <vnfwlq07@naver.com>
2023-12-08 12:34:41 +0900

    .

---
+++ JPA-imgUploadList/src/main/java/com/kh/springdb/config/SecurityConfig.java
@@ -0,0 +1,6 @@
+package com.kh.springdb.config;
+
+public class SecurityConfig {
+	//패스워드 암호화
+	//session과 같은 내용들을 작성
+}
---
+++ JPA-imgUploadList/src/main/java/com/kh/springdb/config/auth/PrincipalDetails.java
@@ -0,0 +1,6 @@
+package com.kh.springdb.config.auth;
+
+public class PrincipalDetails {
+	//유저에 대한 인증이나 유저 정보를 가지고 오는 것을 사용함
+	
+}
